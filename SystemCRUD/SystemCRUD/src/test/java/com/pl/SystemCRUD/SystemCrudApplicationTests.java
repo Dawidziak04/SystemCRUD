db6d352a35f9f0ae3efb5bb51524b56d46d69890
Dawidziak04 <dziakdawid333@wp.pl>
2025-03-25 01:00:49 +0100

    V1.0.1

---
+++ SystemCRUD/SystemCRUD/src/test/java/com/pl/SystemCRUD/SystemCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.SystemCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
